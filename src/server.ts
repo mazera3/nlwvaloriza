3eb3c2184857062f0532a4b81f8010a957240bf2
Edio.Mazera <mazera3@gmail.com>
2022-03-02 13:34:26 -0300

    aula 02

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,20 @@
+import "reflect-metadata";
 import express from "express";
+import { router } from "./routes";
+
+import './database/index'
 
 const app = express();
+app.use(express.json());
 
-/**
- * GET    => Buscar uma informação
- * POST   => Inserir (Criar) uma informação
- * PUT    => Alterar uma informação
- * DELETE => Remover um dado
- * PATCH  => Alterar uma informação específica
- */
+app.use(router);
 
-// app.get('rota','função');
-// GET
-app.get("/test-get", (request, response) => {
-  // Request => Entrando
-  // Response => Saindo
-  return response.send("Teste GET!!!");
-});
-// POST
-app.post("/test-post", (request, response) => {
-    return response.send("Teste POST!!!");
-  });
+app.get('/', (req,res) =>{
+  return res.send("Teste GET!!!");
+})
 
-// executar: yarn dev OU npm dev
-// http://localhost:3000
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running in http://localhost:${PORT}`);
+// executar: yarn dev
+
+app.listen(3000, () => {
+  console.log('Server is running in http://localhost:3000');
 });
